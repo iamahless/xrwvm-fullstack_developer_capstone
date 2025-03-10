6167fd3995205e213df54c77db6675497de2bf84
Alexander <alexandergaruba96@gmail.com>
2025-03-10 16:10:30 +0100

    Refactor code formatting in models, populate, urls, and views for improved readability

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -23,7 +23,8 @@ class CarModel(models.Model):
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=10, choices=TYPE_CHOICES, default='SUV')
     year = models.IntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=True, null=True)
 
     def __str__(self):
         return f"CarModel(name='{self.name}', car_type='{self.type}', year={self.year}, make='{self.car_make.name}')"
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,16 +1,10 @@
-# Uncomment the required imports before adding the code
-
 import json
 import logging
 
-from django.contrib import messages
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth import logout
 from django.contrib.auth.models import User
-from django.http import HttpResponseRedirect, HttpResponse
 from django.http import JsonResponse
-from django.shortcuts import get_object_or_404, render, redirect
-from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
 from .models import CarMake, CarModel
@@ -111,7 +105,6 @@ def get_dealer_reviews(request, dealer_id):
         reviews = get_request(endpoint)
         for review_detail in reviews:
             response = analyze_review_sentiments(review_detail['review'])
-            print(response)
             review_detail['sentiment'] = response['sentiment']
         return JsonResponse({"status": 200, "reviews": reviews})
     else:
@@ -127,8 +120,8 @@ def add_review(request):
     if (request.user.is_anonymous == False):
         data = json.loads(request.body)
         try:
-            response = post_review(data)
-            return JsonResponse({"status": 200})
+            post_review(data)
+            return JsonResponse({"status": 200, "message": "Review posted successfully"})
         except:
             return JsonResponse({"status": 401, "message": "Error in posting review"})
     else:
@@ -143,5 +136,6 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append({"CarModel": car_model.name,
+                    "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
